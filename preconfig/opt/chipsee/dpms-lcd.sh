7e8f6dfc5c7183247ba6fbcd3cd1457ac8a4ef0d
Xiaoqiang Liu <lxq@chipsee.com>
2021-06-23 14:27:26 +0800

    Repair dpms-lcd service have error after customer disable display blank

--- preconfig/opt/chipsee/dpms-lcd.sh
+++ preconfig/opt/chipsee/dpms-lcd.sh
@@ -16,18 +16,18 @@ while [ 1 ]; do
 	MONITORSTATUS=`xset -display :0 q | grep Monitor | awk -F ' ' '{print $3}'`
 
 	CURBRIGHTNESSPATH="/sys/class/backlight/pwm-backlight/brightness"
-	CURBRIGHTNESS=`cat $CURBRIGHTNESSPATH`
+	CURBRIGHTNESS=`cat ${CURBRIGHTNESSPATH}`
 
 	#echo dpms status is $DPMSSTATUS
 	#echo monitor status is $MONITORSTATUS
 	#echo cur bri is $CURBRIGHTNESS
 
-	if [ $DPMSSTATUS = "Enabled" -a $MONITORSTATUS = "Off" -a $CURBRIGHTNESS -gt 0 ]; then
-		SAVEBRIGHTNESS=$CURBRIGHTNESS
-		echo 0 > $CURBRIGHTNESSPATH
+	if [ "x${DPMSSTATUS}" = "xEnabled" -a "x${MONITORSTATUS}" = "xOff" -a ${CURBRIGHTNESS} -gt 0 ]; then
+		SAVEBRIGHTNESS=${CURBRIGHTNESS}
+		echo 0 > ${CURBRIGHTNESSPATH}
 		echo "Close LCD Backlight"
-	elif [ $DPMSSTATUS = "Enabled" -a $MONITORSTATUS = "On" -a $CURBRIGHTNESS -eq 0 ]; then 
-		echo $SAVEBRIGHTNESS > $CURBRIGHTNESSPATH
+	elif [ "x${DPMSSTATUS}" = "xEnabled" -a "x${MONITORSTATUS}" = "xOn" -a ${CURBRIGHTNESS} -eq 0 ]; then 
+		echo ${SAVEBRIGHTNESS} > ${CURBRIGHTNESSPATH}
 		echo "Open LCD Backlight"
 	fi	
 		
